b0c84637269f22c29f80bc7396e403b1e371da1b
Android <shricahrd.in@gmail.com>
2020-05-11 19:17:31 +0530

    3commit

--- app/src/main/java/ch/randhir/circleci/android/demo/MainActivity.java
+++ app/src/main/java/ch/randhir/circleci/android/demo/MainActivity.java
@@ -3,6 +3,8 @@ package ch.randhir.circleci.android.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ch.anoop.circleci.android.demo.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
